e5a668f816f2b6580db1ad5343ae4f4917274bbe
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2025-12-31 11:37:01 +0530

    Update more data vehicles.data.js

--- Select_Your_Ride_V5/data/vehicles.data.js
+++ Select_Your_Ride_V5/data/vehicles.data.js
@@ -51,3 +51,54 @@ const VEHICLES = [
     skillLevel: "beginner"
   }
 ];
+
+{
+  id: "honda_cb350",
+  brand: "Honda",
+  model: "CB 350",
+  type: "bike",
+
+  ergonomics: {
+    seatHeight: 800,
+    posture: "upright",
+    wheelSize: 19
+  },
+
+  usage: {
+    city: 65,
+    highway: 85
+  },
+
+  physical: {
+    kerbWeight: 181,
+    groundClearance: 166
+  },
+
+  segment: "cruiser",
+  skillLevel: "beginner"
+},
+{
+  id: "honda_cb300f",
+  brand: "Honda",
+  model: "CB300F",
+  type: "bike",
+
+  ergonomics: {
+    seatHeight: 789,
+    posture: "sport",
+    wheelSize: 17
+  },
+
+  usage: {
+    city: 75,
+    highway: 70
+  },
+
+  physical: {
+    kerbWeight: 153,
+    groundClearance: 177
+  },
+
+  segment: "street",
+  skillLevel: "intermediate"
+}
