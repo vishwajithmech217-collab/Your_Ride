d31ed42aa219aabfa8c8afc596905a21568d9424
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2025-12-31 16:10:37 +0530

    Update know.js

--- Know_Your_Ride/know.js
+++ Know_Your_Ride/know.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("know.js running");
-
-  console.log("VEHICLES:", typeof VEHICLES, VEHICLES?.length);
-
-  const brandSelect = document.getElementById("brandSelect");
-
-  const brands = [...new Set(VEHICLES.map(v => v.brand))];
-
-  brands.forEach(brand => {
-    const option = document.createElement("option");
-    option.value = brand;
-    option.textContent = brand;
-    brandSelect.appendChild(option);
-  });
-});
+alert("JS IS RUNNING");
