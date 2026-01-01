03a020aa701b8c61685f22aaf9b4ca49469b79f0
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2026-01-01 12:14:38 +0530

    Add VEHICLES array and alert statements

--- Know_Your_Ride/know.js
+++ Know_Your_Ride/know.js
@@ -1,4 +1,8 @@
+window.VEHICLES = [
+  { brand: "Yamaha", model: "MT-15", type: "bike", segment: "street", id: "mt15" },
+  { brand: "Honda", model: "CB350", type: "bike", segment: "cruiser", id: "cb350" }
+];
+
 alert("JS running");
 alert("VEHICLES type: " + typeof VEHICLES);
 alert("Vehicles count: " + VEHICLES.length);
-
