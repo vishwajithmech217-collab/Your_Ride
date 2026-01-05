cbe5dbe1c3a388e467cf37b9a48bba4d7b17ba3b
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2026-01-05 11:30:15 +0530

    Update more bike and scooter vehicles.data.js

--- Select_Your_Ride_V5/data/vehicles.data.js
+++ Select_Your_Ride_V5/data/vehicles.data.js
@@ -1,80 +1,260 @@
 window.VEHICLES = [
 
-  /* ================= BIKES ================= */
+/* ======================
+   YAMAHA BIKES
+====================== */
 
-  {
-    id: "yamaha_mt15",
-    brand: "Yamaha",
-    model: "MT-15",
-    type: "bike",
-    ergonomics: { seatHeight: 810, posture: "sport", wheelSize: 17 },
-    usage: { city: 70, highway: 80 },
-    physical: { kerbWeight: 141, groundClearance: 170 },
-    segment: "street",
-    skillLevel: "intermediate"
-  },
+{
+  id: "yamaha_mt15",
+  brand: "Yamaha",
+  model: "MT-15",
+  type: "bike",
+  ergonomics: { seatHeight: 810 },
+  usage: { city: 70, highway: 80 },
+  physical: { kerbWeight: 141 }
+},
 
-  {
-    id: "re_classic_350",
-    brand: "Royal Enfield",
-    model: "Classic 350",
-    type: "bike",
-    ergonomics: { seatHeight: 805, posture: "upright", wheelSize: 19 },
-    usage: { city: 60, highway: 85 },
-    physical: { kerbWeight: 195, groundClearance: 170 },
-    segment: "cruiser",
-    skillLevel: "beginner"
-  },
+{
+  id: "yamaha_r15_v4",
+  brand: "Yamaha",
+  model: "R15 V4",
+  type: "bike",
+  ergonomics: { seatHeight: 815 },
+  usage: { city: 55, highway: 85 },
+  physical: { kerbWeight: 142 }
+},
 
-  {
-    id: "honda_cb350",
-    brand: "Honda",
-    model: "CB 350",
-    type: "bike",
-    ergonomics: { seatHeight: 800, posture: "upright", wheelSize: 19 },
-    usage: { city: 65, highway: 85 },
-    physical: { kerbWeight: 181, groundClearance: 166 },
-    segment: "cruiser",
-    skillLevel: "beginner"
-  },
+{
+  id: "yamaha_fzs_v4",
+  brand: "Yamaha",
+  model: "FZ-S V4",
+  type: "bike",
+  ergonomics: { seatHeight: 790 },
+  usage: { city: 80, highway: 65 },
+  physical: { kerbWeight: 135 }
+},
 
-  {
-    id: "yamaha_r15_v4",
-    brand: "Yamaha",
-    model: "R15 V4",
-    type: "bike",
-    ergonomics: { seatHeight: 815, posture: "sport", wheelSize: 17 },
-    usage: { city: 55, highway: 85 },
-    physical: { kerbWeight: 142, groundClearance: 170 },
-    segment: "sport",
-    skillLevel: "intermediate"
-  },
+{
+  id: "yamaha_fzx",
+  brand: "Yamaha",
+  model: "FZ-X",
+  type: "bike",
+  ergonomics: { seatHeight: 810 },
+  usage: { city: 75, highway: 65 },
+  physical: { kerbWeight: 139 }
+},
 
-  /* ================= SCOOTERS ================= */
+/* ======================
+   HONDA BIKES
+====================== */
 
-  {
-    id: "honda_activa_6g",
-    brand: "Honda",
-    model: "Activa 6G",
-    type: "scooter",
-    ergonomics: { seatHeight: 765, posture: "relaxed", wheelSize: 12 },
-    usage: { city: 93, highway: 35 },
-    physical: { kerbWeight: 106, groundClearance: 162 },
-    segment: "family_scooter",
-    skillLevel: "beginner"
-  },
+{
+  id: "honda_sp125",
+  brand: "Honda",
+  model: "SP 125",
+  type: "bike",
+  ergonomics: { seatHeight: 790 },
+  usage: { city: 85, highway: 60 },
+  physical: { kerbWeight: 117 }
+},
 
-  {
-    id: "tvs_ntorq_125",
-    brand: "TVS",
-    model: "Ntorq 125",
-    type: "scooter",
-    ergonomics: { seatHeight: 770, posture: "sport", wheelSize: 12 },
-    usage: { city: 90, highway: 40 },
-    physical: { kerbWeight: 118, groundClearance: 155 },
-    segment: "performance_scooter",
-    skillLevel: "beginner"
-  },
+{
+  id: "honda_unicorn",
+  brand: "Honda",
+  model: "Unicorn",
+  type: "bike",
+  ergonomics: { seatHeight: 798 },
+  usage: { city: 75, highway: 70 },
+  physical: { kerbWeight: 140 }
+},
+
+{
+  id: "honda_cb200x",
+  brand: "Honda",
+  model: "CB 200X",
+  type: "bike",
+  ergonomics: { seatHeight: 810 },
+  usage: { city: 65, highway: 80 },
+  physical: { kerbWeight: 147 }
+},
+
+{
+  id: "honda_cb350",
+  brand: "Honda",
+  model: "CB 350",
+  type: "bike",
+  ergonomics: { seatHeight: 800 },
+  usage: { city: 65, highway: 85 },
+  physical: { kerbWeight: 181 }
+},
+
+/* ======================
+   TVS BIKES
+====================== */
+
+{
+  id: "tvs_apache_160_4v",
+  brand: "TVS",
+  model: "Apache RTR 160 4V",
+  type: "bike",
+  ergonomics: { seatHeight: 800 },
+  usage: { city: 75, highway: 70 },
+  physical: { kerbWeight: 146 }
+},
+
+{
+  id: "tvs_apache_200_4v",
+  brand: "TVS",
+  model: "Apache RTR 200 4V",
+  type: "bike",
+  ergonomics: { seatHeight: 800 },
+  usage: { city: 70, highway: 75 },
+  physical: { kerbWeight: 152 }
+},
+
+{
+  id: "tvs_raider_125",
+  brand: "TVS",
+  model: "Raider 125",
+  type: "bike",
+  ergonomics: { seatHeight: 780 },
+  usage: { city: 85, highway: 60 },
+  physical: { kerbWeight: 123 }
+},
+
+{
+  id: "tvs_ronin_225",
+  brand: "TVS",
+  model: "Ronin 225",
+  type: "bike",
+  ergonomics: { seatHeight: 795 },
+  usage: { city: 65, highway: 80 },
+  physical: { kerbWeight: 159 }
+},
+
+/* ======================
+   BAJAJ BIKES
+====================== */
+
+{
+  id: "bajaj_pulsar_125",
+  brand: "Bajaj",
+  model: "Pulsar 125",
+  type: "bike",
+  ergonomics: { seatHeight: 790 },
+  usage: { city: 80, highway: 60 },
+  physical: { kerbWeight: 140 }
+},
+
+{
+  id: "bajaj_pulsar_n160",
+  brand: "Bajaj",
+  model: "Pulsar N160",
+  type: "bike",
+  ergonomics: { seatHeight: 795 },
+  usage: { city: 70, highway: 75 },
+  physical: { kerbWeight: 154 }
+},
+
+{
+  id: "bajaj_pulsar_n250",
+  brand: "Bajaj",
+  model: "Pulsar N250",
+  type: "bike",
+  ergonomics: { seatHeight: 795 },
+  usage: { city: 65, highway: 80 },
+  physical: { kerbWeight: 162 }
+},
+
+{
+  id: "bajaj_dominar_400",
+  brand: "Bajaj",
+  model: "Dominar 400",
+  type: "bike",
+  ergonomics: { seatHeight: 800 },
+  usage: { city: 55, highway: 90 },
+  physical: { kerbWeight: 193 }
+},
+
+/* ======================
+   ROYAL ENFIELD
+====================== */
+
+{
+  id: "re_classic_350",
+  brand: "Royal Enfield",
+  model: "Classic 350",
+  type: "bike",
+  ergonomics: { seatHeight: 805 },
+  usage: { city: 60, highway: 85 },
+  physical: { kerbWeight: 195 }
+},
+
+{
+  id: "re_hunter_350",
+  brand: "Royal Enfield",
+  model: "Hunter 350",
+  type: "bike",
+  ergonomics: { seatHeight: 790 },
+  usage: { city: 70, highway: 75 },
+  physical: { kerbWeight: 181 }
+},
+
+{
+  id: "re_meteor_350",
+  brand: "Royal Enfield",
+  model: "Meteor 350",
+  type: "bike",
+  ergonomics: { seatHeight: 765 },
+  usage: { city: 60, highway: 85 },
+  physical: { kerbWeight: 191 }
+},
+
+/* ======================
+   SCOOTERS
+====================== */
+
+{
+  id: "honda_activa_6g",
+  brand: "Honda",
+  model: "Activa 6G",
+  type: "scooter",
+  ergonomics: { seatHeight: 765 },
+  usage: { city: 90, highway: 40 },
+  physical: { kerbWeight: 106 }
+},
+
+{
+  id: "tvs_jupiter",
+  brand: "TVS",
+  model: "Jupiter",
+  type: "scooter",
+  ergonomics: { seatHeight: 765 },
+  usage: { city: 90, highway: 40 },
+  physical: { kerbWeight: 109 }
+},
+
+{
+  id: "tvs_ntorq_125",
+  brand: "TVS",
+  model: "Ntorq 125",
+  type: "scooter",
+  ergonomics: { seatHeight: 770 },
+  usage: { city: 85, highway: 45 },
+  physical: { kerbWeight: 118 }
+},
+
+{
+  id: "suzuki_access_125",
+  brand: "Suzuki",
+  model: "Access 125",
+  type: "scooter",
+  ergonomics: { seatHeight: 773 },
+  usage: { city: 88, highway: 42 },
+  physical: { kerbWeight: 104 }
+}
+
+];
 
   /* ================= CARS ================= */
 
