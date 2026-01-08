7aff40b7b2723df32692588bc82eacc22a11197e
vishwajithmech217-collab <vishwajithmech217@gmail.com>
2026-01-08 08:47:16 +0530

    Update brands.data.js

--- Know_Your_Ride/data/brands.data.js
+++ Know_Your_Ride/data/brands.data.js
@@ -1,11 +1,30 @@
 window.BRANDS = [
-
-{
-  id: "yamaha_mt15",
-  name: "MT-15",
-  type: "Bike",
-  category: "Street",
-  engine: "155cc",
-  launchYear: 2020,
-  era: "bs6"
-}
+  {
+    brand: "Honda",
+    models: [
+      {
+        id: "honda_activa",
+        name: "Activa",
+        type: "Scooter",
+        category: "Scooter",
+        engine: "110cc",
+        launchYear: 2021,
+        era: "bs6"
+      }
+    ]
+  },
+  {
+    brand: "Yamaha",
+    models: [
+      {
+        id: "yamaha_mt15",
+        name: "MT-15",
+        type: "Bike",
+        category: "Street",
+        engine: "155cc",
+        launchYear: 2020,
+        era: "bs6"
+      }
+    ]
+  }
+];
